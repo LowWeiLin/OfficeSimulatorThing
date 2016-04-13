defac5dab75d45dbfad87505c743f076fc3a0139
Low WeiLin <lwl1991@gmail.com>
2016-04-13 21:55:51 +0800

    Remove misc, fix typo

--- src/State.java
+++ src/State.java
@@ -1,7 +1,8 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class State {
-    private List<Event> events = new ArrayList<Events>();
+    private List<Event> events = new ArrayList<>();
     public State transition() {
         return this;
     }
