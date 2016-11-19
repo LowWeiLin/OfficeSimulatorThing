b93062e89de59293b7a2f065a8d0540a5f0aaf5c
Kang Hong Jin <kanghongjin@gmail.com>
2016-11-19 23:51:56 +0800

    Implement pair's equals,hashcode and toString

--- src/main/java/com/officelife/Pair.java
+++ src/main/java/com/officelife/Pair.java
@@ -1,5 +1,7 @@
 package com.officelife;
 
+import java.util.Objects;
+
 public class Pair<T, K> {
     public final T first;
     public final K second;
@@ -7,5 +9,32 @@ public class Pair<T, K> {
     public Pair(T first, K second) {
         this.first = first;
         this.second = second;
+    }
+
+    public Pair(Pair<T, K> copy) {
+        this(copy.first, copy.second);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+
+        if (getClass() != o.getClass())
+            return false;
+
+        Pair<T, K> other = (Pair<T, K>) o;
+        return Objects.equals(other.first, this.first)
+                && Objects.equals(other.second, this.second);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(first, second);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Pair of (%s, %s)", this.first, this.second);
     }
 }
