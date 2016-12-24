19e1125ec206d7839018fa978030acca015a865f
Darius Foo <darius.foo.tw@gmail.com>
2016-12-24 13:16:40 +0800

    State changes

--- prolog/coffee.pl
+++ prolog/coffee.pl
@@ -1,4 +1,10 @@
 
+:- dynamic person/1.
+:- dynamic location/1.
+:- dynamic adjacent/2.
+:- dynamic has/2.
+:- dynamic at/2.
+
 %% World state
 
 person(ben).
@@ -13,12 +19,16 @@ adjacent(pantry, meeting_room).
 adjacent(meeting_room, board_room).
 
 has(ben, macbook).
+has(pantry, coffee_cup).
 
-at(coffee_cup, pantry).
 at(ben, board_room).
 at(charles, cubicle).
 
 %% Utility
+
+facts :-
+  Relevant = [person, location, adjacent, has, at],
+  maplist(listing, Relevant).
 
 % Adjacency is symmetric, but not reflexive
 is_adjacent(A, B) :-
@@ -42,14 +52,34 @@ can_reach_(Person, Place, Been, [Adj|Path]) :-
 
 can_take(Person, Object, How) :-
   location(Place),
-  at(Object, Place),
+  has(Place, Object),
   can_reach(Person, Place, Path),
-  Movement =.. [move|Path],
-  How = [Movement, take(Person, Object)].
+  maplist(move_to(Person), Path, Movement),
+  append(Movement, [take(Person, Place, Object)], How).
+
+move_to(Person, Place, move(Person, Place)).
+
+%% State changes
+
+update_state([]).
+update_state([Action|Rest]) :-
+  update_state(Action, Sub, Add),
+  maplist(retract, Sub),
+  maplist(assert, Add),
+  update_state(Rest).
+
+update_state(move(Person, Place), [Sub], [Add]) :-
+  Sub =.. [at, Person, _],
+  Add =.. [at, Person, Place].
+
+update_state(take(Person, From, Object), [Sub], [Add]) :-
+  Sub =.. [has, From, Object],
+  Add =.. [has, Person, Object].
 
 %% Rules
 
 drink_coffee(Person, How) :-
   person(Person), location(Place),
   (has(Person, coffee_cup), How = [];
-    at(coffee_cup, Place), can_take(Person, coffee_cup, How)).
+    has(Place, coffee_cup), can_take(Person, coffee_cup, How)).
+
