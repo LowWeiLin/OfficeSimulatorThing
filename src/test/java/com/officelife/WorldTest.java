b406d5d628f021bb9fc926116951f1a7abd0b5a6
Kang Hong Jin <kanghongjin@gmail.com>
2017-03-18 23:44:14 +0800

    add maximum number of steps for findPath

--- src/test/java/com/officelife/WorldTest.java
+++ src/test/java/com/officelife/WorldTest.java
@@ -14,9 +14,25 @@ import static org.hamcrest.MatcherAssert.assertThat;
  */
 public class WorldTest extends TestCase {
 
-    public void testFindPath() {
+    public void testFindPathReturns() {
         World world = new World();
-        List<Coords> path = world.findPath(new Coords(-3, 0), new World.EndCoords(new Coords(3, 1)));
+        List<Coords> path = world.findPath(new Coords(-3, 0), new World.EndCoords(new Coords(3, 1))).get();
         assertThat(path, Matchers.anything());
+    }
+
+    public void testFindPathTerminatesBeforeMaxSteps() {
+        World world = new World();
+
+        boolean zeroStep = world.findPath(new Coords(0, 0), new World.EndCoords(new Coords(0, 0)), 0)
+                .isPresent();
+        assertThat(zeroStep, Matchers.is(true));
+
+        boolean singleStep = world.findPath(new Coords(0, 0), new World.EndCoords(new Coords(0, 1)), 1)
+                .isPresent();
+        assertThat(singleStep, Matchers.is(true));
+
+        boolean twoSteps = world.findPath(new Coords(0, 0), new World.EndCoords(new Coords(0, 2)), 1)
+                .isPresent();
+        assertThat(twoSteps, Matchers.is(false));
     }
 }
