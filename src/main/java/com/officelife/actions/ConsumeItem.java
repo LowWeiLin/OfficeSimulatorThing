982247697deee714541575b39e3eedd478a9acfa
Kang Hong Jin <kanghongjin@gmail.com>
2017-03-19 00:35:26 +0800

    simplify consumeItem

--- src/main/java/com/officelife/actions/ConsumeItem.java
+++ src/main/java/com/officelife/actions/ConsumeItem.java
@@ -1,9 +1,6 @@
 package com.officelife.actions;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
+import java.util.Optional;
 
 import com.officelife.goals.State;
 import com.officelife.items.Item;
@@ -19,26 +16,20 @@ public class ConsumeItem<T> extends Action {
 
   @Override
   public boolean accept() {
-    Map<Boolean, List<Item>> partitioned = state.actor.inventory().stream().collect(
-      Collectors.groupingBy(itemClass::isInstance, Collectors.toList()));
+    Optional<Item> maybe = state.actor.inventory().stream()
+            .filter(itemClass::isInstance)
+            .findFirst();
 
-    if (!partitioned.containsKey(true) || partitioned.get(true).isEmpty()) {
-      // failed to consume
+    if (!maybe.isPresent()) {
       return false;
     }
 
-    List<Item> notRemoved = new ArrayList<>(partitioned.get(true));
+    Item item = maybe.get();
 
     // TODO perform effects for the item that was just removed
     // TODO better data structure
-    notRemoved.remove(0);
 
-    state.actor.inventory().clear();
-    if (partitioned.containsKey(false)) {
-      state.actor.inventory().addAll(partitioned.get(false));
-    }
-    state.actor.inventory().addAll(notRemoved);
-
+    state.actor.inventory().remove(item);
     return true;
   }
 
